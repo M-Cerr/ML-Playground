2ef5257a8834bc8c723bba556b152e80242443c8
M-Cerr <007454935@coyote.csusb.edu>
2024-10-10 17:17:04 -0700

    making heroku setup stuff

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title("ML PLayground")
 st.write("Welcome to the ML Playground Tool :)")
+
+st.sidebar.title("Preprocessing Options")
+st.sidebar.checkbox("Missing Value Replacement")
+st.sidebar.checkbox("Scaling")
